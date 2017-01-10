5ae659968b9ffbeca17185f90f238035955fe62c
Waseem999 <wakarim2@gmail.com>
2017-01-10 11:03:26 -0600

    suscriptionscheduleboxes

---
+++ app/components/SubscriptionScheduleClass.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+
+const SubscriptionSchedule = () => (
+    <div>
+        <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Monday
+            </label>
+        </div>
+         <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Tuesday
+            </label>
+        </div>
+         <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Wednesday
+            </label>
+        </div>
+         <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Thursday
+            </label>
+        </div>
+         <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Friday
+            </label>
+        </div>
+        <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Saturday
+            </label>
+        </div>
+         <div className="checkbox">
+            <label>
+                <input
+                onChange={}
+                type="checkbox"
+                id="checkbox"/>
+                Sunday
+            </label>
+        </div>
+        
+    </div>
+);
+
+export default SubscriptionSchedule;
