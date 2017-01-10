ff0f9155b20732adff3ddd8c96b043ca24284430
Alan H <alherman10@gamil.com>
2017-01-10 16:13:25 -0600

    fixed bugs

--- app/components/accessdenied.jsx
+++ app/components/accessdenied.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router';
 export default function(){
     return (
         <div>
-            <img src="APP/public/access_denied.jpg" alt="DENIED"/>
+            <img src="/access_denied.jpg" alt="DENIED"/>
             <Link to="/" >Home</Link>
         </div>
     )
