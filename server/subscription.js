08bcf12f6c23108c2573257ad7ad374d9f725642
Hal Carleton <hello@halleycarleton.com>
2017-01-10 17:05:02 -0600

    Fixing whitespace

--- server/subscription.js
+++ server/subscription.js
@@ -8,24 +8,25 @@ const bodyParser = require('body-parser');
 
 
 router.get('/', (req, res, next) => {
-   return Subscription.findOne({ 
-    where: { 
-      userId: req.session.userId
-    }
-  })
+  return Subscription.findOne({ 
+      where: { 
+        userId: req.session.userId
+      }
+    })
     .then(subscription => {
-    res.json(subscription)})
-  .catch(next);
+      res.json(subscription)
+    })
+    .catch(next);
 });
 
 router.put('/', (req, res, next) => {
    Subscription.findOne({
-      where : {
-        userId : req.session.userId
-      }
-    })
-    .then(subscription => {
-      subscription.update({frequency : req.body.dayselected})
-    })
-  .catch(next);
+       where : {
+         userId : req.session.userId
+       }
+     })
+     .then(subscription => {
+       subscription.update({frequency : req.body.dayselected})
+     })
+     .catch(next);
 });
