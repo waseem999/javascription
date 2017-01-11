f541b71471c0163877ac01c3f8399e473bb48a04
Hal Carleton <hello@halleycarleton.com>
2017-01-11 10:34:46 -0600

    Splitting server/start.js into two files

---
+++ tests/server/index.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const app = require('../../server');
+const chai = require('chai');
+const supertest = require('supertest');
+
+const expect = chai.expect;
+
+describe('Express App', () => {
+  it('should run without throwing errors', () => {
+
+  });
+});
