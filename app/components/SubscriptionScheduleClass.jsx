9f5c06a6388808b9ccc3b7d4c8a1400c061811e7
Hal Carleton <hello@halleycarleton.com>
2017-01-12 10:22:44 -0600

    Creating basic test for <SubscriptionSchedule />

--- app/components/SubscriptionScheduleClass.jsx
+++ app/components/SubscriptionScheduleClass.jsx
@@ -1,48 +1,47 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-
 export const SubscriptionSchedule = () => (
     <div>
         <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Monday
             </label>
         </div>
          <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Tuesday
             </label>
         </div>
          <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Wednesday
             </label>
         </div>
          <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Thursday
             </label>
         </div>
          <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Friday
             </label>
         </div>
         <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Saturday
             </label>
         </div>
          <div className="checkbox">
             <label>
-                <input onChange={} type="checkbox" id="checkbox"/>
+                <input type="checkbox" id="checkbox"/>
                 Sunday
             </label>
         </div>
@@ -50,12 +49,11 @@ export const SubscriptionSchedule = () => (
 );
 
 function mapStateToProps(state){
-  let dayselected = state.subscription.dayselected;
-  let time = state.subscription.time;
-
+  const dayselected = state.subscription.dayselected;
+  const time = state.subscription.time;
 
   return {
-    dayselected, time 
+    dayselected, time
   }
 }
 
@@ -63,9 +61,6 @@ function mapDispatchToProps(state){
   return {
 
   }
-};
-
+}
 
 export default connect (mapStateToProps, mapDispatchToProps)(SubscriptionSchedule);
-
-
