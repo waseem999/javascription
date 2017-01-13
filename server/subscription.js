0eed74aaadd5f383cb21351f57e3dcc85168cb39
Hal Carleton <hello@halleycarleton.com>
2017-01-13 14:04:51 -0600

    Removing empty where object

--- server/subscription.js
+++ server/subscription.js
@@ -46,11 +46,7 @@ router.put('/days', (req, res, next) => {
 });
 
 router.get('/days', (req, res, next) => {
-  Subscription.findOne({
-    where : {
-
-    }
-  })
+  Subscription.findOne()
 })
 
 module.exports = router;
