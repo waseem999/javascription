bfd6d32602b9795ad6f91dfaf02c6568dfd678da
Alan H <alherman10@gamil.com>
2017-01-16 13:28:07 -0600

    added filter

--- app/components/allcoffees.jsx
+++ app/components/allcoffees.jsx
@@ -13,12 +13,14 @@ class SelectedCoffees extends Component{
                 tier2: [],
                 tier3: []
             },
+            filter: 'all'
         }
 
         this.handleCoffeeClick = this.handleClick.bind(this);
         this.reconcileCoffees = this.reconcileCoffees.bind(this);
         this.parseCoffees = this.parseCoffees.bind(this);
         this.findCoffee = this.findCoffee.bind(this);
+        this.filterChange = this.filterChange.bind(this);
     }
 
     reconcileCoffees(selected, tier){
@@ -79,70 +81,94 @@ class SelectedCoffees extends Component{
         })
     }
 
+    filterChange(e){
+        this.setState({filter: e.target.value});
+    }
+
 
     render(props){
         return (
             <div>
                 <div>
+                    <select name="filter" id="filter" value={this.state.filter} onChange={this.filterChange}>
+                        <option value="all">All</option>
+                        <option value="1">Tier 1</option>
+                        <option value="2">Tier 2</option>
+                        <option value="3">Tier 3</option>
+                    </select>
+                </div>
+                {   this.state.filter === 'all' || this.state.filter === '1' ?
+                    <div>
                     <h3>Tier 1 Coffees. $1/cup $15/lb</h3>
-                {
-                    this.state.allCoffees.tier1.map(function(coffee){
-                        return (
-                            <span key={coffee.name}>
-                                <div>
-                                    <img src={`${coffee.photo}`} alt="Tier 1 coffee"/>
-                                </div>
-                                <div>
-                                    <span>{coffee.name}</span>
-                                </div>
-                                <div>
-                                    <button name={coffee.name} tier={coffee.tier} onClick={this.handleCoffeeClick}>Add to Subscription</button>
-                                </div>
-                            </span>
-                        )
-                    })
+                    {
+                        this.state.allCoffees.tier1.map(function(coffee){
+                            return (
+                                <span key={coffee.name}>
+                                    <div>
+                                        <img src={`${coffee.photo}`} alt="Tier 1 coffee"/>
+                                    </div>
+                                    <div>
+                                        <span>{coffee.name}</span>
+                                    </div>
+                                    <div>
+                                        <button name={coffee.name} tier={coffee.tier} onClick={this.handleCoffeeClick}>Add to Subscription</button>
+                                    </div>
+                                </span>
+                            )
+                        })
+                    }
+                    </div>
+                    :
+                    null
                 }
-                </div>
-                <div>
-                    <h3>Tier 2 Coffees. $2.50/cup $25/lb</h3>
-                {
-                    this.state.allCoffees.tier2.map(function(coffee){
-                        return (
-                            <span key={coffee.name}>
-                                <div>
-                                    <img src={`${coffee.photo}`} alt="Tier 2 coffee"/>
-                                </div>
-                                <div>
-                                    <span>{coffee.name}</span>
-                                </div>
-                                <div>
-                                    <button name={coffee.name} tier={coffee.tier} onClick={this.handleCoffeeClick}>Add to Subscription</button>
-                                </div>
-                            </span>
-                        )
-                    })
+                {   this.state.filter === 'all' || this.state.filter === '2' ?
+                    <div>
+                        <h3>Tier 2 Coffees. $2.50/cup $25/lb</h3>
+                    {
+                        this.state.allCoffees.tier2.map(function(coffee){
+                            return (
+                                <span key={coffee.name}>
+                                    <div>
+                                        <img src={`${coffee.photo}`} alt="Tier 2 coffee"/>
+                                    </div>
+                                    <div>
+                                        <span>{coffee.name}</span>
+                                    </div>
+                                    <div>
+                                        <button name={coffee.name} tier={coffee.tier} onClick={this.handleCoffeeClick}>Add to Subscription</button>
+                                    </div>
+                                </span>
+                            )
+                        })
+                    }
+                    </div>
+                    :
+                    null
                 }
-                </div>
-                <div>
-                    <h3>Tier 3 Coffees. $4/cup $35/lb</h3>
-                {
-                    this.state.allCoffees.tier3.map(function(coffee){
-                        return (
-                            <span key={coffee.name}>
-                                <div>
-                                    <img src={`${coffee.photo}`} alt="Tier 3 coffee"/>
-                                </div>
-                                <div>
-                                    <span>{coffee.name}</span>
-                                </div>
-                                <div>
-                                    <button name={coffee.name} tier={coffee.tier} onClick={this.handleCoffeeClick}>Add to Subscription</button>
-                                </div>
-                            </span>
-                        )
-                    })
+                {   this.state.filter === 'all' || this.state.filter === '3' ?
+                    <div>
+                        <h3>Tier 3 Coffees. $4/cup $35/lb</h3>
+                    {
+                        this.state.allCoffees.tier3.map(function(coffee){
+                            return (
+                                <span key={coffee.name}>
+                                    <div>
+                                        <img src={`${coffee.photo}`} alt="Tier 3 coffee"/>
+                                    </div>
+                                    <div>
+                                        <span>{coffee.name}</span>
+                                    </div>
+                                    <div>
+                                        <button name={coffee.name} tier={coffee.tier} onClick={this.handleCoffeeClick}>Add to Subscription</button>
+                                    </div>
+                                </span>
+                            )
+                        })
+                    }
+                    </div>
+                    :
+                    null
                 }
-                </div>
             </div>
         )
     }
